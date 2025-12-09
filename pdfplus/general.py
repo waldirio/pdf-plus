968e935f9a63b3eb50654890a66dd79a6adce2d7
Waldirio M Pinheiro <waldirio@gmail.com>
2025-12-09 09:03:45 -0800

    adding general class

---
+++ pdfplus/general.py
@@ -0,0 +1,27 @@
+from flask import render_template
+
+class General():
+
+    def apology(self, message, code=400):
+        """
+        Render message as an apology to user.
+        """
+        def escape(s):
+            """
+            Escape special characters.
+
+            https://github.com/jacebrowning/memegen#special-characters
+            """
+            for old, new in [
+                ("-", "--"),
+                (" ", "-"),
+                ("_", "__"),
+                ("?", "~q"),
+                ("%", "~p"),
+                ("#", "~h"),
+                ("/", "~s"),
+                ('"', "''"),
+            ]:
+                s = s.replace(old, new)
+            return s
+        return render_template("apology.html", top=code, bottom=escape(message)), code
